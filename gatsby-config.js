8a34af8495170aca9bdc246edeec0ae6a0197928
Kim Persson <kimpersson88@gmail.com>
2020-01-01 17:41:53 +0100

    FIXES clean up gatsby boilerplate and add temporary contact page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Hacketh`,
+    description: `Hacketh blockchain development agency`,
+    author: `Kim Persson`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
